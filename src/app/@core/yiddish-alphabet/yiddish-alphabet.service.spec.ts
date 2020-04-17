1ca741d0e1c8c77060608175c1fd62fe6b4f5338
Roni Gal <gotlib@gmail.com>
2020-04-17 04:21:15 +0200

    yiddish-alphabet service test

--- src/app/@core/yiddish-alphabet/yiddish-alphabet.service.spec.ts
+++ src/app/@core/yiddish-alphabet/yiddish-alphabet.service.spec.ts
@@ -1,57 +1,104 @@
-import { TestBed, async, fakeAsync } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import {
+  HttpClientTestingModule,
+  HttpTestingController
+} from '@angular/common/http/testing';
 import { YiddishAlphabetService } from './yiddish-alphabet.service';
-import { YiddishAlphabetClient } from './yiddish-alphabet.model';
 import { UserSettingsService } from '../user-settings/user-settings.service';
 import { of, Subscriber, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
 
 describe('YiddishAlphabetService', () => {
   let abService: YiddishAlphabetService;
   let uSservice: UserSettingsService;
-  // let httpTestingController: HttpTestingController;
-  // let userSettingsSpy;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [
-        UserSettingsService,
-      ]
+      providers: [UserSettingsService]
     });
     abService = TestBed.inject(YiddishAlphabetService);
-    // httpTestingController = TestBed.inject(HttpTestingController);
+    httpTestingController = TestBed.inject(HttpTestingController);
     uSservice = TestBed.inject(UserSettingsService);
     spyOn(uSservice, 'language$').and.returnValue(of('en'));
   });
 
   afterEach(() => {
-    // httpTestingController.verify();
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(abService).toBeTruthy();
   });
 
-  it('should have an data$ observable', () => {
-    expect(abService.alphabet$()).toBeInstanceOf (Observable);
+  it('should have a alphabet$ observable', () => {
+    expect(abService.alphabet$()).toBeInstanceOf(Observable);
   });
 
   it('should call the userSettings service', () => {
+    const req1Mock = [
+      {
+        yiddishLetter: 'אַ',
+        letterName: 'Pasekh Alef'
+      },
+      {
+        yiddishLetter: 'אָ',
+        letterName: 'Komets Alef'
+      },
+      {
+        yiddishLetter: 'ב',
+        letterName: 'Beys'
+      }
+    ];
+    const req2Mock = [
+      {
+        yiddishLetter: 'אַ',
+        foreignLetter: ['a']
+      },
+      {
+        yiddishLetter: 'אָ',
+        foreignLetter: ['o']
+      },
+      {
+        yiddishLetter: 'ב',
+        foreignLetter: ['b']
+      }
+    ];
 
-    // spyOn(abService, 'alphabet$').and.returnValue(of());
+    abService.alphabet$().subscribe((res) => {
+      expect(res.length).toBe(3);
 
-    // TODO: get mock jsons and merge them with foreign property
-    // abService.alphabet$().subscribe();
+      expect(res[0]).toEqual(
+        jasmine.objectContaining({
+          yiddishLetter: 'אַ',
+          letterName: 'Pasekh Alef',
+          foreignLetter: ['a']
+        })
+      );
+
+      expect(res[2]).toEqual(
+        jasmine.objectContaining({
+          yiddishLetter: 'ב',
+          letterName: 'Beys',
+          foreignLetter: ['b']
+        })
+      );
+
+      expect(res[2].foreignLetter[0]).toEqual('b');
+    });
+
+    const req1 = httpTestingController.expectOne(
+      `${environment.mocks}yiddish-alphabet.json`
+    );
+    const req2 = httpTestingController.expectOne(
+      `${environment.mocks}foreign-letters/en.json`
+    );
+
+    expect(req1.request.method).toEqual('GET');
+    expect(req2.request.method).toEqual('GET');
+
+    req1.flush(req1Mock);
+    req2.flush(req2Mock);
   });
-
-  xit('should return an Observable<TestLetters[]> object from HTTP', () => {
-    // const req = httpTestingController.expectOne(
-    //   `${environment.mocks}foreign-letters/en.json`
-    // );
-    // expect(req.request.method).toBe('GET');
-
-    // req.flush(testLettersMock);
-  });
-
-
 });
